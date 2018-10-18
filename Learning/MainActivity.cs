ece2220d8fc2b5c93dd80a2bb0460a2cde2da4b3
allenly3 <allenly3@163.com>
2018-10-17 20:42:46 -0400

    adapter

--- Learning/MainActivity.cs
+++ Learning/MainActivity.cs
@@ -3,17 +3,40 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Collections.Generic;
 
 namespace Learning
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+        private List<string> mItems;
+        private ListView mListView;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+           
+            mItems = new List<string>();
+            mItems.Add("Bob");
+            mItems.Add("Tom");
+            mItems.Add("Jim");
+                    
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this ,Android.Resource.Layout.SimpleListItem1,mItems);
+
+            mListView.Adapter = adapter;
+
+
+
+
         }
     }
 }
